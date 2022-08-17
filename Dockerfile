be8858a664829e347eb9ff63b977e47327f30afa
sharhan-alhassan <sharhanalhassan@gmail.com>
2022-08-17 12:38:54 +0000

    patch: v0.0.1 exclude collections

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 # Container image that runs your code
 FROM alpine:3.10
 
+RUN apk update \
+    apk add --no-cache --upgrade mongodb mongodb-tools \
+
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
-#!/bin/sh -l
+#!/bin/sh 
+source_cluster=$1
+destination_cluster=$2
+database=$3
+exclude_collection=$4
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+mongodump --uri=${source_cluster}/${database} --excludeCollection=${exclude_collection} --archive | mongorestore --uri=${destination_cluster} --archive
+
+# echo "Hello $1"
+# time=$(date)
+# echo "::set-output name=time::$time"
